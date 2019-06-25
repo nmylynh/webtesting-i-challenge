d8e9cccc75a04e122e6cd86a27d9a35244dd06dc
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-24 20:23:20 -0700

    added succed and repair

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,7 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return { ...item, enhancement: checkLevel(item.enhancement)};
 }
 
 function fail(item) {
@@ -14,9 +14,15 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
+}
+
+function checkLevel () {
+  enhancement < 20
+  ? enhancement + 1
+  : enhancement
 }
