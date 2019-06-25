d7e1eb9fb8cb81ef6158d0ea2121a0026b5dd77d
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-24 20:52:39 -0700

    finished writing all functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,23 +6,52 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item, enhancement: checkLevel(item.enhancement)};
+  return { ...item, 
+    enhancement: upgradeELvl(item.enhancement)
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item, 
+    enhancement: downgradeELvl(item.enhancement), 
+    durability: ferghus(item) 
+  };
 }
 
 function repair(item) {
-  return { ...item, durability: 100 };
+  return { ...item, 
+    durability: 100 
+  };
 }
 
 function get(item) {
-  return { ...item };
+  return { ...item,
+    name: showELvl(item) 
+  };
 }
 
-function checkLevel (enhancement) {
+//helpers
+
+function upgradeELvl(enhancement) {
   enhancement < 20
   ? enhancement + 1
   : enhancement
+}
+
+function ferghus(item) {
+  item.enhancement < 15
+  ? item.durability - 5
+  : item.durability - 10
+}
+
+function downgradeELvl(enhancement) {
+  enhancement > 16
+  ? enhancement - 1
+  : enhancement
+}
+
+function showELvl(item){
+  item.enhancement > 0
+  ? `${item.name} [+${item.enhancement}]`
+  : item.name
 }
